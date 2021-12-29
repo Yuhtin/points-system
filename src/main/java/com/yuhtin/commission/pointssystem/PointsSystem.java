db4d559550f24f4030c32c259523d7678ac39c13
Yuhtin <rio.daviduarte@gmail.com>
2021-12-29 01:33:16 -0300

    feat: init project

---
+++ src/main/java/com/yuhtin/commission/pointssystem/PointsSystem.java
@@ -0,0 +1,17 @@
+package com.yuhtin.commission.pointssystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PointsSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
